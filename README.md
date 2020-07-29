a827f36ff52ef201db03ded0aea7a2b85140f65e
Sergii Shilingov <s.shilingov@gmail.com>
2020-07-29 13:10:39 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Project setup
+
+```
+npm install
+```
+
+Add your Firebase project credentials to `src/firebase.js`
+
+```
+npm run serve
+```
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" class="app">
+    <header>
+      <h1 class="app-title">Vue Firebase App</h1>
+      <p>Realtime Walkie-Talkie Voice Chat</p>
+    </header>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+header {
+  text-align: center;
+}
+button, input {
+  margin-bottom: 10px;
+}
+</style>
+
+<style scoped>
+.app {
+  margin: 20px auto 0 auto;
+  padding-bottom: 20px;
+  max-width: 600px;
+  border: 1px solid #ebebeb;
+  border-radius: 0.42rem;
+}
+
+.app-title {
+  margin: 20px 0;
+  padding: 0;
+}
+</style>
